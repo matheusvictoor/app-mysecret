2591a2241c34943e5e5ebdabfe0f7a6b1b4cd5cb
Matheus Victor <matheusvictor.dev@gmail.com>
2024-02-12 02:18:26 -0300

    feat: add dao classes

--- api/src/infra/database/dao/AnswerDAO.ts
+++ api/src/infra/database/dao/AnswerDAO.ts
@@ -1,6 +1,27 @@
+import { AnswerModel } from '@domain/model';
 import DAO from '@domain/dao/DAO';
-import AnswerEntity from '@domain/entity/AnswerEntity';
+import { KnexTypeAdapter, DatabaseTableNames } from '../KnexAdapter';
 
-// export default class AnswerDAO implements DAO<AnswerEntity> {
+export default class AnswerDAO implements DAO<AnswerModel> {
+    private readonly tableName: string = DatabaseTableNames.ANSWER;
 
-// }
+    constructor(private readonly connection: KnexTypeAdapter) {}
+
+    async create(data: AnswerModel): Promise<AnswerModel> {
+        const [savedUser] = await this.connection<AnswerModel>(this.tableName)
+            .insert(data)
+            .returning('*');
+
+        return savedUser;
+    }
+
+    async findById(answerId: string): Promise<AnswerModel | null> {
+        const data = await this.connection<AnswerModel>(this.tableName)
+            .where({ answerId })
+            .first();
+
+        if(!data) return null;
+
+        return data;
+    }
+}
--- api/src/infra/database/dao/QuestionDAO.ts
+++ api/src/infra/database/dao/QuestionDAO.ts
@@ -1,6 +1,27 @@
+import { QuestionModel } from '@domain/model';
 import DAO from '@domain/dao/DAO';
-import QuestionEntity from '@domain/entity/QuestionEntity';
+import { KnexTypeAdapter, DatabaseTableNames } from '../KnexAdapter';
 
-// export default class QuestionDAO implements DAO<QuestionEntity> {
+export default class QuestionDAO implements DAO<QuestionModel> {
+    private readonly tableName: string = DatabaseTableNames.QUESTIONS;
 
-// }
+    constructor(private readonly connection: KnexTypeAdapter) {}
+
+    async create(data: QuestionModel): Promise<QuestionModel> {
+        const [savedUser] = await this.connection<QuestionModel>(this.tableName)
+            .insert(data)
+            .returning('*');
+
+        return savedUser;
+    }
+
+    async findById(questionId: string): Promise<QuestionModel | null> {
+        const data = await this.connection<QuestionModel>(this.tableName)
+            .where({ questionId })
+            .first();
+
+        if(!data) return null;
+
+        return data;
+    }
+}
